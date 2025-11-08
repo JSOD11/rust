395477f477e5931a78a09f71e29ed27a821595f3
Justin O'Dwyer <justinsodwyer@gmail.com>
2025-11-08 15:25:57 -0500

    Refactoring.

--- projects/minigrep/src/main.rs
+++ projects/minigrep/src/main.rs
@@ -1,15 +1,37 @@
-use std::{env, fs};
+use std::{env, error::Error, fs, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem building config: {}", err);
+        process::exit(1);
+    });
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
+}
 
-    let query = &args[1];
-    let file_path = &args[2];
-
-    println!("Searching for: {:#?}", query);
-    println!("File path: {:#?}", file_path);
-
-    let file_contents = fs::read_to_string(file_path).expect("Should have been able to read file");
-
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let file_contents =
+        fs::read_to_string(config.file_path)?;
+    println!("Query: {:#?}", config.query);
     println!("File contents: {:#?}", file_contents);
+    Ok(())
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    pub fn build(args: &[String]) -> Result<Config, &str> {
+        if args.len() != 3 {
+            return Err("2 arguments required: <search_term>, <path_to_file>");
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+        Ok(Self { query, file_path })
+    }
 }
